71656e969bc61938a2060d9fba046ee20b13c938
Pedro Minicz <pedrominicz@gmail.com>
2020-09-01 13:11:37 -0300

    Improve `Test.v`.

--- Test.v
+++ Test.v
@@ -4,7 +4,7 @@ Definition surjective {A B} (f : A -> B) := forall b, exists a, f a = b.
 Definition left_inverse {A B} (f : A -> B) g := forall a, g (f a) = a.
 Definition right_inverse {A B} (f : A -> B) g := forall b, f (g b) = b.
 
-Theorem left_inverse_injective : forall {A B} (f : A -> B),
+Theorem left_inverse_injective : forall {A B} {f : A -> B},
   (exists g, left_inverse f g) -> injective f.
 Proof.
   unfold injective, left_inverse.
@@ -14,7 +14,7 @@ Proof.
   assumption.
 Qed.
 
-Theorem right_inverse_surjective : forall {A B} (f : A -> B),
+Theorem right_inverse_surjective : forall {A B} {f : A -> B},
   (exists g, right_inverse f g) -> surjective f.
 Proof.
   unfold right_inverse, surjective.
